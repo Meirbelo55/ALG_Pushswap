3a1f49b6e3aca0b12fd5b910429d71c362ae48c5
meir <meir.belaich@epitech.eu>
2020-07-26 18:59:58 +0200

    operation terminee

--- push_swap.php
+++ push_swap.php
@@ -1,29 +1,32 @@
 <?php
 
-    //if($argv != 3) {
-       // print('this program will make your count number order by ');
-    //}
-
   //first array
-  $la = [0,1,2,3,4,5,6,7,8,9];
+  $la = $argv;
+  array_shift($la);
   //second array
-  $lb = [ 5,1,3,2,4,0,6,7,8,9];
-
+  $lb = [];
+ //sa
     function switchTheFirstTwoInLa()
     {
         global $la;
         $temp = $la[0];
         $la[0] = $la[1];
         $la[1] = $temp;
+        //print_r($la);
     }
     switchTheFirstTwoInLa();
 
-    function switchTheFirstTwoInLb()
-    {
+    function switchTheFirstTwoInLb()  {
         global $lb;
-        $tem = $lb[0];
-        $lb[0] = $lb[1];
-        $lb[1] = $tem;
+        if($lb == null) {
+            return null;
+        }
+        else {
+            $tem = $lb[0];
+            $lb[0] = $lb[1];
+            $lb[1] = $tem;
+            print_r($lb);
+        }
     }
      switchTheFirstTwoInLb();
 
@@ -34,48 +37,104 @@
         $temp = $la[0];
         $la[0] = $la[1];
         $la[1] = $temp;
-
-        $temp = $lb[0];
-        $lb[0] = $lb[1];
-        $lb[1] = $temp;
+        print_r($la);
+            if($lb == null) {
+                return null;
+            }
+            else {
+                $tem = $lb[0];
+                $lb[0] = $lb[1];
+                $lb[1] = $tem;
+                print_r($lb);
+            }
     }
-     switchTheFirstTwoLaLbTogether();
+    switchTheFirstTwoLaLbTogether();
 
-    //moveElementToAnotherArray
     function moveElement() {
         global $la;
         global $lb;
         $num = 0;
         foreach($la  as $key => $value) {
-            if (++$num > 2) break;
+            if (++$num > 1) break;
                 $lb[$key] = $value;
                 unset($la[$key]);
             }
+        print_r($la);
+        print_r($lb);
     }
     moveElement();
 
      //switch first element to the last
-    function switchFirstToLast() {
+    function switchFirstToLastLa() {
         global $la;
+        $first = array_shift($la);
         $last = end($la);
-        //print_r($last);
-        $la[0] = $last;
-        echo $last;
+        //$last = end($la);
+        //$last = $first;
+        array_push($la,$first);
+        print_r($la);
+
     }
-    switchFirstToLast();
+    switchFirstToLastLa();
 
-
-//$array1= $la;
-//array_push($array1,$argv[1]);
-//print_r($array1);
-
-function  numberCount($data) {
-    echo $data . "\n";
+function switchFirstToLastLb() {
+    global $lb;
+    $first = array_shift($lb);
+    $last = end($lb);
+    //$last = end($la);
+    array_push($lb,$first);
+    print_r($lb);
 }
-//numberCount(1,2,3,4,5,6);
+  switchFirstToLastLb();
+
+function switchFirstToLastBoth() {
+    global $la;
+    global $lb;
+    //for la array
+    $firstLa = array_shift($la);
+    array_push($la,$firstLa);
+    //for lb array
+    $firstLb = array_shift($lb);
+    array_push($lb,$firstLb);
+
+}
+    switchFirstToLastBoth();
+
+    function switchLastToFirstLa() {
+        global $la;
+       // $last = end($la);
+        $first = $la[0];
+        //$la[0] = $last;
+        array_unshift($la,array_pop($la));
+        print_r($la);
+    }
+    switchLastToFirstLa();
+
+function switchLastToFirstLb() {
+    global $lb;
+    // $last = end($la);
+    $first = $lb[0];
+    //$la[0] = $last;
+    array_unshift($lb,array_pop($lb));
+    print_r($lb);
+}
+    switchLastToFirstLb();
+
+function switchLastToFirstBoth() {
+    global $la;
+    global $lb;
+    $firstLa = $la[0];
+    $first = $lb[0];
+    array_unshift($la,array_pop($la));
+    array_unshift($lb,array_pop($lb));
+    print_r($lb);
+}
+
+switchLastToFirstBoth();
 
 
-          //la  = 2 8 5 6 0 1 4 3 7 8 9
+
+//la  = 2 8 5 6 0 1 4 3 7 8 9
          //lb = [4 *   ra  +  rra]
             // sa : switch the 2 first number of the la
              //sb : switch the 2 first number of the lb
